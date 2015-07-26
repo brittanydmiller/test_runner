d51acb88681477dec24cbb68e008f0d1865e5711
Brittany Miller <brittanydmiller@gmail.com>
2015-07-26 15:28:36 -0700

    Rough pass at OOJS

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
-function generateDummyTest() {
-  var delay = 1000 + Math.random() * 2000;
-  var testPassed = Math.random() > 0.5;
+// function generateDummyTest() {
+//   var delay = 1000 + Math.random() * 2000;
+//   var testPassed = Math.random() > 0.5;
 
-  return function(callback) {
-    setTimeout(function() {
-      callback(testPassed);
-    }, delay);
-  };
-}
+//   return function(callback) {
+//     setTimeout(function() {
+//       callback(testPassed);
+//     }, delay);
+//   };
+// }
 
 var tests = [
   { description: "commas are rotated properly",          run: generateDummyTest() },
@@ -20,12 +20,40 @@ var tests = [
 
 function runTests(){
   for ( test in tests ) {
-    console.log(test);
-    console.log(tests);
-    tests[test].run( function(){
-        console.log(arguments[0])
-      });
+    // console.log(test);
+    // console.log(tests);
+    // tests[test].run( function(){
+    //     console.log(arguments[0])
+    //   });
+    var myTest = new Test(tests[test]);
+    myTest.run( function(){
+      this.render;
+    })
   }
+}
+
+function Test(test){
+  this.status = "Not Started Yet";
+  this.description = test.description;
+};
+
+Test.prototype = {
+  constructor: Test,
+
+  run: function(){ this.generateDummyTest() },
+  render: function(){
+    console.log(arguments[0]); 
+  },
+  generateDummyTest: function() {
+    var delay = 1000 + Math.random() * 2000;
+    var testPassed = Math.random() > 0.5;
+
+    return function(callback) {
+      setTimeout(function() {
+        callback(testPassed);
+      }, delay);
+  };
+}
 }
 
 window.onload = function(){
