b83788f0f63d7c31952e4519bff2575502f8269d
Brittany Miller <brittanydmiller@gmail.com>
2015-07-26 15:04:51 -0700

    Set up access to results of each test

--- js/main.js
+++ js/main.js
@@ -1,1 +1,34 @@
+function generateDummyTest() {
+  var delay = 1000 + Math.random() * 2000;
+  var testPassed = Math.random() > 0.5;
+
+  return function(callback) {
+    setTimeout(function() {
+      callback(testPassed);
+    }, delay);
+  };
+}
+
+var tests = [
+  { description: "commas are rotated properly",          run: generateDummyTest() },
+  { description: "exclamation points stand up straight", run: generateDummyTest() },
+  { description: "run-on sentences don't run forever",   run: generateDummyTest() },
+  { description: "question marks curl down, not up",     run: generateDummyTest() },
+  { description: "semicolons are adequately waterproof", run: generateDummyTest() },
+  { description: "capital letters can do yoga",          run: generateDummyTest() }
+];
+
+function runTests(){
+  for ( test in tests ) {
+    console.log(test);
+    console.log(tests);
+    tests[test].run( function(){
+        console.log(arguments[0])
+      });
+  }
+}
+
+window.onload = function(){
+  document.getElementById("js-test-runner").addEventListener("click", runTests);
+}
 
