534f1b9b1b807944304deac0b36574d13aea2687
Brittany Miller <brittanydmiller@gmail.com>
2015-07-26 18:21:53 -0700

    Update UI and logging as tests finish running.

--- js/main.js
+++ js/main.js
@@ -18,15 +18,46 @@ var tests = [
   { description: "capital letters can do yoga",          run: generateDummyTest() }
 ];
 
+var tests_passed = 0;
+var tests_failed = 0;
+var tests_running = 0;
+var tests_completed = 0;
+
 function runTests(){
-  for ( test in tests ) {
-    console.log(test);
-    console.log(tests);
-    tests[test].run( function(){
-        console.log(arguments[0])
+  tests_running = tests.length;
+  tests.forEach(function(currentValue, index, array){
+    currentValue.run( function(){
+      console.log(index);
+        if(arguments[0] == true) {
+          updateTest("Passed", index);
+          tests_passed += 1;
+        } else {
+          updateTest("Failed", index);
+          tests_failed += 1;
+        }
+          tests_completed += 1;
+          tests_running -= 1;
+          console.log("Tests Passed: " + tests_passed);
+          console.log("Tests Failed: " + tests_failed);
+          console.log("Tests Running: " + tests_running);
+          console.log("Tests Completed: " + tests_completed);
+          if (tests_completed === tests.length) {
+            sayDone();
+          }
       });
-  }
+  }, this);
 }
+
+function updateTest(text, index){
+  document.getElementById("status" + index).innerText = text;
+}
+
+function sayDone(){
+  var final_message = document.createElement("h2");
+  final_message.innerText = "All Tests Completed!"
+  document.getElementById("js-test-container").appendChild(final_message);
+};
+
 
 function loadTests(){
   var container = document.getElementById("js-test-container")
